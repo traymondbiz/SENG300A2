d827cb2b6b91d4c814a36a2859767439576df842
zacharymetz <zachary.metz@ucalgary.ca>
2017-11-10 13:03:51 -0700

    added Test FOlder

---
+++ src/test FOlder/ucalgary/seng300/a2/VendingListener.java
@@ -0,0 +1,93 @@
+package ca.ucalgary.seng300.a2;
+
+import org.lsmr.vending.*;
+import org.lsmr.vending.hardware.*;
+
+/**
+ * This class is registered by VendingManager with hardware classes to listen for hardware
+ * events and perform first-pass checks and error-handling for them. Most "heavy-lifting" 
+ * is completed within VendingManager.
+ * 
+ * ACCESS: Only listener methods are public access. 
+ * 
+ * HANDLED EVENTS: 	SelectionButtonListener: pressed() 
+ *   				CoinSlotListener: ValidCoinInserted()
+ *
+ * @author Raymond Tran (30028473)
+ * @author Thomas Coderre (10169277)
+ * @author Thobthai Chulpongsatorn (30005238)
+ *
+ */
+public class VendingListener implements CoinSlotListener, PushButtonListener {
+	private static VendingListener listener;
+	private static VendingManager mgr;
+	
+	private VendingListener (){}
+	
+	/**
+	 * Forces the existing singleton instance to be replaced.
+	 * Called by VendingManager during its instantiation.
+	 */
+	static void initialize(VendingManager manager){		
+		if (manager != null){
+			mgr = manager;
+			listener = new VendingListener();
+		}
+	}
+	
+	/**
+	 * Provides access to the singleton instance for package-internal classes.
+	 * @return The singleton VendingListener instance  
+	 */
+	static VendingListener getInstance(){
+		return listener;
+	}
+
+	// Currently unneeded listener events.
+	@Override
+	public void coinRejected(CoinSlot slot, Coin coin) {}
+	@Override
+	public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {}
+	@Override
+	public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {}
+
+
+	/**
+	 * Responds to "pressed" notifications from registered SelectionButtons. 
+	 * If no matching button is found in the VendingMachine, nothing is done.
+	 * Uses the buy() method in VendingManager to process the purchase.
+	 * All exceptions thrown by buy() are caught here (InsufficientFunds, Disabled, Empty, etc.) 
+	 */
+	@Override
+	public void pressed(PushButton button) {
+		int bIndex = mgr.getButtonIndex(button); 
+		if (bIndex == -1){
+			//Then it's not a pop selection button. 
+			//This may be where we handle "change return" button presses
+		}
+		else{
+			try{
+				//Assumes a 1-to-1, strictly ordered mapping between popIndex and and butttonindex
+				mgr.buy(bIndex); 
+			} catch(InsufficientFundsException e){
+				//TODO Respond to insufficient funds by printing message to display.
+				// Should use e.toString().
+			} catch(DisabledException e){
+				//TODO Respond to the system being disabled.
+			} catch (EmptyException e){
+				//TODO Respond to the pop rack being empty
+			} catch (CapacityExceededException e){
+				//TODO Respond to the delivery chute being full.
+			}
+		}		
+	}
+
+	/**
+	 * Responds to "Valid coin inserted" notifications from the registered CoinSlot.
+	 * Adds the value of the coin to the VendingManager's tracked credit.
+	 */
+	@Override
+	public void validCoinInserted(CoinSlot slot, Coin coin) {
+		mgr.addCredit(coin.getValue());
+	}
+}
