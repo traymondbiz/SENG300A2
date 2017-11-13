87f12914130469dfb953b007889a3863237c3807
CS\hooman.khosravi1 <hooman.khosravi.72@gmail.com>
2017-11-12 17:45:05 -0700

    Merge branch 'active' of https://github.com/traymondbiz/SENG300A2 into active
    # Conflicts:
    #	src/ca/ucalgary/seng300/a2/LoopingThread.java
    #	src/ca/ucalgary/seng300/a2/VendingManager.java
    #	src/ca/ucalgary/seng300/a2/test/TestCases.java

--- src/ca/ucalgary/seng300/a2/VendingListener.java
+++ src/ca/ucalgary/seng300/a2/VendingListener.java
@@ -1,5 +1,7 @@
 package ca.ucalgary.seng300.a2;
 
+import java.io.IOException;
+
 import org.lsmr.vending.*;
 import org.lsmr.vending.hardware.*;
 
@@ -59,9 +61,11 @@ public class VendingListener implements CoinSlotListener, PushButtonListener, Co
 	 * If no matching button is found in the VendingMachine, nothing is done.
 	 * Uses the buy() method in VendingManager to process the purchase.
 	 * All exceptions thrown by buy() are caught here (InsufficientFunds, Disabled, Empty, etc.) 
+	 * @throws IOException 
 	 */
 	@Override
-	public void pressed(PushButton button) {
+	public void pressed(PushButton button) throws IOException {
+		
 		int bIndex = mgr.getButtonIndex(button); 
 		if (bIndex == -1){
 			//Then it's not a pop selection button. 
@@ -69,16 +73,21 @@ public class VendingListener implements CoinSlotListener, PushButtonListener, Co
 		}
 		else{
 			try{
-				//Assumes a 1-to-1, strictly ordered mapping between popIndex and and butttonindex
+				//Assumes a 1-to-1, strictly ordered mapping betwee
 				mgr.buy(bIndex); 
+				mgr.addLog(mgr.getPopKindName(bIndex)+" button pressed by user with: "+ Integer.toString(mgr.getCredit()));
 			} catch(InsufficientFundsException e){
 				//TODO Respond to insufficient funds by printing message to display.
 				// Should use e.toString().
+				mgr.addLog("User Could not purchase " + mgr.getPopKindName(bIndex) + ". " + Integer.toString(mgr.getPopKindCost(bIndex)-mgr.getCredit()) + " cents missing from credit.");
 			} catch(DisabledException e){
-				//TODO Respond to the system being disabled.
+				//TODO Respond to the system being disabled
+				mgr.addLog("User Could not purchase " + mgr.getPopKindName(bIndex) + " since the system is disabled");
 			} catch (EmptyException e){
+				mgr.addLog("User Could not purchase " + mgr.getPopKindName(bIndex) + " becasue there is none in the machine.");
 				//TODO Respond to the pop rack being empty
 			} catch (CapacityExceededException e){
+				mgr.addLog("User Could not purchase " + mgr.getPopKindName(bIndex) + " becasue the deivery chute is full of change");
 				//TODO Respond to the delivery chute being full.
 			}
 		}		
@@ -87,15 +96,27 @@ public class VendingListener implements CoinSlotListener, PushButtonListener, Co
 	/**
 	 * Responds to "Valid coin inserted" notifications from the registered CoinSlot.
 	 * Adds the value of the coin to the VendingManager's tracked credit.
+	 * @throws IOException 
 	 */
 	@Override
 	public void validCoinInserted(CoinSlot slot, Coin coin) {
 		mgr.addCredit(coin.getValue());
+		try {
+			mgr.addLog("User instered: " + Integer.toString(coin.getValue()) +"coin to coin slot");
+		}catch( IOException e) {
+			
+		}
+		
 	}
 
 	@Override
-	public void coinsDelivered(CoinReturn coinReturn, Coin[] coins) {
+	public void coinsDelivered(CoinReturn coinReturn, Coin[] coins){
 		mgr.resetDisplay();
+		try {
+		mgr.addLog("Coins Returned to User: " + coins.toString());
+		}catch( IOException e) {
+			
+		}
 	}
 	
 	@Override
